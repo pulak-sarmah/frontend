efba10e93d0270b510ecad4ef7b0668e85d7d683
pulak-sarmah <pulaksarmah333@gmail.com>
2024-06-23 12:47:29 +0530

    refactor: Update apolloClient.ts and userStore.ts
    This commit refactors the apolloClient.ts file by adding a WebSocketLink for handling subscriptions and an errorLink for handling authentication errors. It also updates the import statement for createUploadLink to use the correct path. Additionally, the userStore.ts file is added, which defines the user store using Zustand for managing user-related state.

---
+++ src/stores/userStore.ts
@@ -0,0 +1,40 @@
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
+import { User } from "../gql/graphql";
+
+interface UserState {
+  id: number | undefined;
+  avatarUrl: string | null;
+  fullname: string;
+  email?: string;
+  updateProfileImage: (image: string) => void;
+  updateUsername: (name: string) => void;
+  setUser: (user: User) => void;
+}
+
+export const useUserStore = create<UserState>()(
+  persist(
+    (set) => ({
+      id: undefined,
+      avatarUrl: null,
+      fullname: "",
+      email: "",
+
+      updateProfileImage: (image: string) =>
+        set((state) => ({ ...state, avatarUrl: image })),
+      updateUsername: (name: string) =>
+        set((state) => ({ ...state, fullname: name })),
+
+      setUser: (user: User) =>
+        set({
+          id: user.id || undefined,
+          avatarUrl: user.avatarUrl,
+          fullname: user.fullname,
+          email: user.email,
+        }),
+    }),
+    {
+      name: "user-storage",
+    }
+  )
+);
